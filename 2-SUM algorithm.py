c660a7f5865b4e59b4f6cbea35fa1384f8554ebf
user-name <orangisme@gmail.com>
2012-07-28 22:13:55 +0400

    Update 2-SUM algorithm.py

--- 2-SUM algorithm.py
+++ 2-SUM algorithm.py
@@ -1,10 +1,21 @@
 __author__ = 'Z'
-a=[1,2,3,4,5,6,7]
-for i in a[2:6]:
-    print i
-b=set(a)
-print b
+#f = open('HashInt.txt')
+f = open('2sum.txt')
+array_of_integer=[]
 
-for c in range(1,5):
-    print c
+for line in f:
+    array_of_integer.append(int(line))
 
+set_of_integer=set(array_of_integer)
+result=set()
+res=0
+#for t in array_of_integer[2499:4000]:
+for t in range(2500,4001):
+    for i in array_of_integer:
+        r=t-i
+        #if r>0 and r!=i and (r in set_of_integer):
+        if r>0 and (r in set_of_integer):
+            result.add(i)
+            res+=1
+print len(result)
+print res
