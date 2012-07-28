0ad1bda21d8e65e17b6e9d9d230fd92c022d130d
Or Ange <orangisme@gmail.com>
2012-07-28 22:22:05 +0400

    заменил переменные

--- 2-SUM algorithm.py
+++ 2-SUM algorithm.py
@@ -11,11 +11,11 @@ result=set()
 res=0
 #for t in array_of_integer[2499:4000]:
 for t in range(2500,4001):
-    for i in array_of_integer:
-        r=t-i
+    for y in array_of_integer:
+        x=t-y
         #if r>0 and r!=i and (r in set_of_integer):
-        if r>0 and (r in set_of_integer):
-            result.add(i)
+        if x>0 and (x in set_of_integer):
+            result.add(y)
             res+=1
 print len(result)
 print res
