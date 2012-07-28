281ba6858344c56b23b7fb1f418e3a026bba229c
Or Ange <orangisme@gmail.com>
2012-07-28 23:38:51 +0400

    задача решена

--- 2-SUM algorithm.py
+++ 2-SUM algorithm.py
@@ -1,6 +1,6 @@
 __author__ = 'Z'
-#f = open('HashInt.txt')
-f = open('2sum.txt')
+f = open('HashInt.txt')  #otvet: 1477
+#f = open('2sum.txt') #otvet: 1453
 array_of_integer=[]
 
 for line in f:
@@ -8,14 +8,16 @@ for line in f:
 
 set_of_integer=set(array_of_integer)
 result=set()
-res=0
+
 #for t in array_of_integer[2499:4000]:
 for t in range(2500,4001):
     for y in array_of_integer:
         x=t-y
         #if r>0 and r!=i and (r in set_of_integer):
-        if x>0 and (x in set_of_integer):
-            result.add(y)
-            res+=1
+        if x>0 and x!=y and (x in set_of_integer):
+            result.add(t)
+            #print result
+            #print t, y, "x = ",x, "/n"
+
 print len(result)
-print res
+
